f3ed98c74cdcb2db4b8824160290e9678879f700
Tony Villa <tonyvilla77@gmail.com>
2021-01-03 13:39:43 -0800

    hid form functionality

--- script.js
+++ script.js
@@ -112,6 +112,16 @@ class App {
     inputDistance.focus();
   }
 
+  _hideForm() {
+    // Empty inputs
+    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value =
+      '';
+
+    form.style.display = 'none';
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000);
+  }
+
   _toggleElevationField() {
     inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
@@ -172,8 +182,7 @@ class App {
     this._renderWorkout(workout);
 
     // Hide form + Clear input fields
-    inputDistance.value = inputCadence.value = inputDuration.value = inputElevation.value =
-      '';
+    this._hideForm();
   }
 
   // Display marker
